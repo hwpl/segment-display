df4875483b3e2318bd42bb8eea17a9075d1f93b0
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-06-16 00:15:21 +0200

    Fix example in the class documentation

--- src/segment_display.h
+++ src/segment_display.h
@@ -41,7 +41,7 @@ const PROGMEM uint8_t DECODER[] = {
 ///
 /// Driver<4> seg({2, 3, 4, 5}, {6, 7, 8, 9, 10, 11, 12});
 ///
-/// seg.setState({Driver::DASH, 4, 2, Driver::DASH});
+/// seg.setDigits({Driver::DASH, 4, 2, Driver::DASH});
 ///
 /// void loop() {
 ///     seg.refresh();
