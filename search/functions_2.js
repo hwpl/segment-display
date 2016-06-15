ab19450a4aeaddf624cd7014e7de56ed6cdaecdd
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-06-16 00:36:40 +0200

    Update documentation

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setdigit',['setDigit',['../classSegmentDisplay_1_1Driver.html#a86d2c253401c6eec60a950b5986bbff0',1,'SegmentDisplay::Driver']]],
-  ['setdigits',['setDigits',['../classSegmentDisplay_1_1Driver.html#a0068e056fadc2123b658a13996b3f2eb',1,'SegmentDisplay::Driver']]]
+  ['setdigits',['setDigits',['../classSegmentDisplay_1_1Driver.html#a0068e056fadc2123b658a13996b3f2eb',1,'SegmentDisplay::Driver']]],
+  ['setsegments',['setSegments',['../classSegmentDisplay_1_1Driver.html#a545636484836ffb67e4bfdc448ca6224',1,'SegmentDisplay::Driver']]]
 ];
